b73c9307219ead69e5e3407795c0c4106541d595
pablopaezsheridan <pablopaezps97@gmail.com>
2023-10-26 23:26:31 -0300

    Class 3: Authentication controller complete but not working.Missing program settings

---
+++ 2TUP5ConsultaAlumnoAPI/Data/Models/CredentialsDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _2TUP5ConsultaAlumnoAPI.Data.Models
+{
+    public class CredentialsDto
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ 2TUP5ConsultaAlumnoAPI/Services/Implementations/UserService.cs
@@ -0,0 +1,29 @@
+﻿using _2TUP5ConsultaAlumnoAPI.Data;
+using _2TUP5ConsultaAlumnoAPI.Data.Entities;
+
+namespace _2TUP5ConsultaAlumnoAPI.Services.Implementations
+{
+    public class UserService
+    {
+        private readonly ConsultaContext _consultaContext;
+
+        public UserService(ConsultaContext consultaContext)
+        {
+            _consultaContext = consultaContext;
+        }
+
+        public Tuple<bool,User?> ValidarUsuario(string email, string password)
+        {
+            User? userForLogin = _consultaContext.Users.SingleOrDefault(u => u.Email == email);
+            if (userForLogin != null)
+            {
+                if (userForLogin.Password == password)
+                    return new Tuple<bool, User?>(true, userForLogin);
+                return new Tuple<bool, User?>(false, userForLogin);
+            }
+            return new Tuple<bool,User?>(false, null);
+
+
+        }
+    }
+}
